35280456bcd684cd3d0d64cdf6737b21c0af7f2a
ForceITA <cezar.sas@gmail.com>
2015-01-12 15:41:18 +0100

    Commenti

--- ool.lisp
+++ ool.lisp
@@ -1,15 +1,20 @@
-
 ;;;; -*- Mode: Lisp -*-
 ;;;; ool.lisp
 
+
+;; Creo una hash-table
 (defparameter *classes-specs* (make-hash-table))
 
+
+;; ADD-CLASS-SPEC aggiunge un elemento alla
+;; hash-table
 (defun add-class-spec (name class-spec)
   (setf (gethash name *class-specs*) class-spec))
 
+;; GET-CLASS-SPEC data una chiave è una hash-table
+;; restituisce il valore
 (defun get-class-spec (name)
   (gethash name *classes-specs*))
-
 
 (defun process-method (method-name method-spec)
   #| TO-DO Miracle |#
