5658dc4f3bd4a7ac83305b8004189908fc2e7520
WizarDoGemu <arial92@hotmail.com>
2015-01-19 19:49:35 +0100

    Correzione Spaghetti code
    ho riunito le 3 funzioni richieste dal progetto in 3 aree.
    Tutte le funzioni richiamate maggiormente da una funzione principale si
    trovano nella stessa area sopra alle suddette per maggiore leggibilità.

--- ool.lisp
+++ ool.lisp
@@ -4,15 +4,95 @@
 ;; Creo una hash-table
 (defparameter *classes-specs* (make-hash-table))
 
-;; ADD-CLASS-SPEC aggiunge un elemento alla
-;; hash-table
-(defun add-class-spec (name class-spec)
-  (setf (gethash name *classes-specs*) class-spec))
+
+#| definizione di una classe|#
 
 ;; GET-CLASS-SPEC data una chiave e una hash-table
 ;; restituisce il valore
 (defun get-class-spec (name)
   (gethash name *classes-specs*))
+
+;; ADD-CLASS-SPEC aggiunge un elemento alla
+;; hash-table
+(defun add-class-spec (name class-spec)
+  (setf (gethash name *classes-specs*) class-spec))
+
+;;crea una lista di coppie di valori
+(defun create-a-list (n-list)
+  (cond ((null n-list) '())
+        ((oddp (length n-list)) (error "Slot-Vaules not balanced."))
+        ((not (symbolp (first n-list))) (error "Value not a symbol")) 
+        #|Fine controlli|#
+        (t (append (list (cons
+                          (first n-list)
+                          (list (second n-list)))) 
+                   (create-a-list (cddr n-list))))))
+
+;; DEFINE-CLASS costruisce una classe, associa al nome 
+;; della classe una "a-list" siffatta 
+;; ((*PARENT* PARENT) (SLOT-NAME VALUE)*)
+(defun define-class (class-name parent &rest slot-value)
+  (cond ((null (symbolp class-name))
+         (error "Class name is not a Symbol"))
+        ((not (symbolp parent))
+         (error "parent is not a Symbol"))
+        ((not (or
+               (null parent)
+               (not (null (get-class-spec parent)))))
+         (error "Could't find parent"))
+        (t (add-class-spec class-name 
+                           (list (cons 'PARENT  
+                                       parent) 
+                                 (create-a-list 
+                                  slot-value)))))
+  class-name)
+
+
+#|Instanza di una classe|#
+
+;;input:
+;;class-name: nome della classe definita
+;;slot value: value passate in ingresso
+(defun new (class-name &rest slot-value)
+  (cond ((null (get-class-spec class-name)) 
+         (error "Class don't exist"))
+        ((oddp (length slot-value));restituisce true se la lista ha un numero di elementi dispari
+         (error "Slot-Values not balanced"))
+        (t #|(check-slot class-name slot-value)|# 
+         (append (list 'ool-instance class-name) (create-a-list slot-value))
+         #|(error "Slot don't exist in the class")|#)))
+
+
+#| estrazione valore da hash-table|#
+
+#| instance = (ool-instance <class> (list <k v>*)) |#
+#| ((PARENT <class>) (list <k v>*)) |#
+;;estrae il valore di un campo da una classe
+(defun get-slot (instance slot-name)
+  (get-slot-class (append (list (cons 'PARENT 
+                                      (second instance))) 
+                          (rest (rest instance)))
+                  slot-name
+                  t))
+
+(defun get-slot-class (class-list slot-name &optional (print-methodp nil))
+  (let ((key-value (assoc slot-name (rest class-list)));cosa fa?
+        (class (cdr (first class-list))))
+    (cond ((and (null key-value)
+                (null class))
+           (error "Unable to find slot"))
+          ((and (null key-value) class)
+           (get-slot-class (get-class-spec class) slot-name))
+          ((and key-value print-methodp) key-value)
+          ((and key-value 
+                print-methodp
+                (listp (second key-value))
+                #|(eql (first (second key-value)) 'METHOD)|#)
+           key-value)
+          (t (error "Unable to find the slot2 ")))))
+
+
+;;disordine/spazzatura
 
 ;; PROCESS-SLOT-VALUE data una lista, costruisce una 
 ;; "a-list" e in caso di metodi chiama la costruzione
@@ -21,65 +101,29 @@
  (defun process-slot-value (list)
    (if (oddp (length list)) (error "Slot-Values not balanced, expected even found odd"))
    (cond ((null list) ()) 
-        (t (if (and (listp (second list)) 
+         (t (if (and (listp (second list)) 
                     (eql (first (second list)) 'METHOD)) 
                (process-method (first list) (second list)))
            (append (list (cons (first list) (list(second list)))) 
                    (process-slot-value (rest (rest list))))))) 
 |#
 
-(defun create-a-list (n-list)
-  (cond ((null n-list) '())
-        ((oddp (length n-list)) (error "Slot-Vaules not balanced."))
-        ((not (symbolp (first n-list))) (error "Value not a symbol")) 
-        (t (append (list (cons
-                          (first n-list)
-                          (list (second n-list)))) 
-                   (create-a-list (cddr n-list))))))
-
 #| (defun process-method (method-name method-spec)
      #| TO-DO Miracle |#
      (eval (rewrite-method-code method-name method-spec)))|#
 
 #| STUB FUNCTION - USE TOP |#
-(defun process-method (method-name method-spec)
-  (print method-spec))
+;(defun process-method (method-name method-spec)  (print method-spec))
 
-;; DEFINE-CLASS costruisce una classe, associa al nome 
-;; della classe una "a-list" siffatta 
-;; ((*PARENT* PARENT) (SLOT-NAME VALUE)*)
-(defun define-class (class-name parent &rest slot-value)
-  (if (symbolp class-name)
-      (if (symbolp parent)
-          (if (or
-               (null parent)
-               (not (null (get-class-spec parent))))
-              (add-class-spec class-name (list (cons "PARENT"  parent) (create-a-list slot-value)))
-            (error "Could't find parent"))
-        (error "parent is not a Symbol"))
-    (error "Class name is not a Symbol"))
-  class-name)
+;(defun check-slot (class-name slot-value)  )
 
-(defun new (class-name &rest slot-value)
-  (cond ((null (get-class-spec class-name)) (error "Class don't exist"))
-        ((oddp (length slot-value)) (error "Slot-Values not balanced"))
-        (t #|(check-slot class-name slot-value)|# 
-         (append (list 'ool-instance class-name) (create-a-list slot-value))
-         #|(error "Slot don't exist in the class")|#)))
 
-(defun check-slot (class-name slot-value)
-  )
 
-#| instance = (ool-instance <class> (list <k v>*)) |#
+;;class-list lista contenente il secondo valore della cons instance 
+;;slot-name quote contenente la spec da richiamare
 
-(defun get-slot (instance slot-name)
-  (get-slot-class (append 
-                   (list (cons 
-                          'PARENT 
-                          (second instance))) 
-                   (third instance)) slot-name t))
 
-#| NON FUNZIONA |#
+#|NON FUNZIONA 
 (defun get-slot-class (class slot-name &optional (print-method nil))
   (let ((key-value (assoc slot-name (cdr class))))
     (cond ((and key-value print-method) key-value)
@@ -87,5 +131,5 @@
           ((null (cdr class))
            (error "Could not find slot"))
           (t (get-slot-class (get-class-spec (cdar class)) slot-name)))))
-
+|#
 ;;;; End of file ool.lisp
