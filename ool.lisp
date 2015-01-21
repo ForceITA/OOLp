3422aebe46b2dcbce5c57300f32c6933d5e98551
ForceITA <cezar.sas@gmail.com>
2015-01-21 11:01:17 +0100

    Aggiunto check-slot

--- ool.lisp
+++ ool.lisp
@@ -49,77 +49,46 @@
 
 #|Instanza di una classe|#
 
-;;input:
-;;class-name: nome della classe definita
-;;slot value: value passate in ingresso
+
 (defun new (class-name &rest slot-value)
   (cond ((null (get-class-spec class-name)) 
          (error "Class don't exist"))
         ((oddp (length slot-value));restituisce true se la lista ha un numero di elementi dispari
          (error "Slot-Values not balanced"))
-        (t #|(check-slot class-name slot-value)|# 
-         (append (list 'ool-instance class-name) (create-a-list slot-value))
-         #|(error "Slot don't exist in the class")|#)))
+        (t (check-slot class-name slot-value) 
+           (append (list 'ool-instance class-name) (create-a-list slot-value)))))
 
 
-#| estrazione valore da hash-table|#
-
-#| instance = (ool-instance <class> (list <k v>*)) |#
-#| ((PARENT <class>) (list <k v>*)) |#
-;;estrae il valore di un campo da una classe
+;; GET-SLOT estrae il valore di un campo da un istanza
 (defun get-slot (instance slot-name)
-  (get-slot-class (append (list (cons 'PARENT 
-                                      (second instance))) 
-                          (list (rest (rest instance))))
-                  slot-name))
+  (second (get-slot-class (append (list (cons 'PARENT 
+                                             (second instance))) 
+                                 (list (rest (rest instance))))
+                         slot-name)))
 
+(defun check-slot (class slot-value) 
+  (cond ((null slot-value) t)
+        ((and (= 2 (length slot-value))
+              (let ((key-value (get-slot-class 
+                                (list (cons 'PARENT class)) 
+                                (first slot-value))))
+                (if (and (listp (second key-value)) 
+                         (eql (first (second key-value)) 
+                              'METHOD))
+                    (error "Unable to override a method"))))
+         t)
+        (t (check-slot class (cddr slot-value))))) 
+
+;; GET-slot estrae il valore di un campo da un classe
 (defun get-slot-class (class-list slot-name) 
   (let ((key-value (assoc slot-name (cadr class-list))) ;assegna a key-value la coppia chiave valore se esiste
-        (class (cdr (first class-list))));assegna a class il parent della classe
+        (parent (cdr (first class-list))));assegna a class il parent della classe
     (cond ((and (null key-value)
-                (null class))
-           (error "Unable to find slot"))
-          ((and (null key-value) class)
-           (get-slot-class (get-class-spec class ) 
+                (null parent))
+           (error "Unable to find slot")); La chiave non esiste e non ha parent
+          ((and (null key-value) parent)
+           (get-slot-class (get-class-spec parent) 
                            slot-name));se non esiste la coppia chiave e valore e la classe e' diversa da nil chiama get slot class, passando class, e slot-name senza specificare print method
-          (t key-value))))
+          (key-value key-value))))
 
-
-;;disordine/spazzatura
-
-;; PROCESS-SLOT-VALUE data una lista, costruisce una 
-;; "a-list" e in caso di metodi chiama la costruzione
-;; del metodo
-#|
-   (defun process-slot-value (list)
-     (if (oddp (length list)) (error "Slot-Values not balanced, expected even found odd"))
-     (cond ((null list) ()) 
-         (t (if (and (listp (second list)) 
-                     (eql (first (second list)) 'METHOD)) 
-                (process-method (first list) (second list)))
-           (append (list (cons (first list) (list(second list)))) 
-                   (process-slot-value (rest (rest list))))))) 
-   |#
-
-#| (defun process-method (method-name method-spec)
-     #| TO-DO Miracle |#
-     (eval (rewrite-method-code method-name method-spec)))|#
-
-#| STUB FUNCTION - USE TOP |#
-;(defun process-method (method-name method-spec)  (print method-spec))
-
-;(defun check-slot (class-name slot-value)  )
-
-;;class-list lista contenente il secondo valore della cons instance 
-;;slot-name quote contenente la spec da richiamare
-
-#|NON FUNZIONA 
-(defun get-slot-class (class slot-name &optional (print-method nil))
-  (let ((key-value (assoc slot-name (cdr class))))
-    (cond ((and key-value print-method) key-value)
-          ((and key-value (eql (cdar key-value) 'METHOD)) (error "Cannot override a method in an instance"))  
-          ((null (cdr class))
-           (error "Could not find slot"))
-          (t (get-slot-class (get-class-spec (cdar class)) slot-name)))))
-|#
 ;;;; End of file ool.lisp
